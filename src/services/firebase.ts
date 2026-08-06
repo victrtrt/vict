d4eae618c036737750f24a4309f02eefa86fbaf3
victrtrt <lutfullahhabibi032@gmail.com>
2026-08-06 03:59:03 -0700

    Create firebase.ts

---
+++ src/services/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZPtyqjEejOjQblJftcvTUha_Ft7RyJEY",
+  authDomain: "vict-dc9dc.firebaseapp.com",
+  projectId: "vict-dc9dc",
+  storageBucket: "vict-dc9dc.firebasestorage.app",
+  messagingSenderId: "205504802411",
+  appId: "1:205504802411:web:30068b16d95c9a4c38cb2e"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
