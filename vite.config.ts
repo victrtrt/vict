05946e84817a809c52ff6c235ecce9722729e363
victrtrt <lutfullahhabibi032@gmail.com>
2026-08-06 05:41:24 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    tsconfigPaths: true,
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  plugins: [react()],
 });
